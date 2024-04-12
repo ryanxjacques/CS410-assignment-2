d2619ce0a4a5e5b88579fc6abe08d4c4a79bdd3d
Ryan Jacques <ryanxjacques@gmail.com>
2024-04-12 15:18:35 -0700

    Added the ParticleModifier script, but still not getting the correct info for collisions

--- Assets/UnityTechnologies/3DBeginnerComplete/Scripts/ParticleModifier.cs
+++ Assets/UnityTechnologies/3DBeginnerComplete/Scripts/ParticleModifier.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class ParticleModifier : MonoBehaviour
 {
     // Start is called before the first frame update
+    ParticleSystem particleSystem;
     void Start()
     {
-
+        particleSystem = GetComponent<ParticleSystem>();
     }
 
-    
+    void OnCollisionEnter(Collision other) {
+        Debug.Log("Collided with: " + other.collider.gameObject.name); // Log the name of the collided object
+        if (other.collider.tag == "Player") {
+            Debug.Log("Collision with specific tag detected!");
+            var mainModule = particleSystem.main;
+            mainModule.startColor = new ParticleSystem.MinMaxGradient(Color.red);
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
