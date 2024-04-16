b1efb80b84f08cfb4f21d58dfe64846cea42485d
CgvO <cgv@uoregon.edu>
2024-04-16 08:38:40 -0700

    cleaned up code, added descriptions to painting script

--- Assets/UnityTechnologies/3DBeginnerComplete/Scripts/Painting.cs
+++ Assets/UnityTechnologies/3DBeginnerComplete/Scripts/Painting.cs
@@ -17,14 +17,19 @@ public class Painting : MonoBehaviour
         // checks to see if the player collides with the object
         if(other.transform == player)
         {
+            // gets position of player in a really roundabout way
             var playerObject = play;
             Vector3 pos = playerObject.transform.position;
             float hor = pos.x;
             float ver = pos.z;
+
+            // uses dot product to check that the player is in a certain corner of the box collider
             if((1.0f*hor)+(0.0f*ver) < -18.5f){
+                
+                // teleports the player above the map (not sure why they don't fall, since gravity is
+                // enabled, but it doesn't make too much of a difference)
                 player.transform.position = new Vector3(13.0f,15.0f,1.5f);
             }
-            //player.transform.position = new Vector3(13.0f,15.0f,1.5f);
         }
     }
 }
